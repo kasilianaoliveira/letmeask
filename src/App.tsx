e83f76f8f1a9a94de55b889ac4cf22927541ca92
kasilianaoliveira <kasilianaoliveira@gmail.com>
2022-04-23 22:50:35 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { createContext, useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
